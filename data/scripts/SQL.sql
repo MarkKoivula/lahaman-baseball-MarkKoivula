78fde9e684a233eb3cb23e3f34481c76611d4155
MarkKoivula <mkoivula6515@gmail.com>
2020-08-31 21:33:53 -0500

    updated #7, began working on #10-13

--- data/scripts/SQL.sql
+++ data/scripts/SQL.sql
@@ -10,7 +10,7 @@
 --       and t.table_type = 'BASE TABLE'
 -- order by t.table_schema;
 
-SELECT distinct year 
+SELECT DISTINCT year 
 FROM public.homegames
 ORDER BY year ASC -- 1871 to 2016
 
@@ -26,29 +26,31 @@ FROM information_schema.tables t
 INNER JOIN information_schema.columns c 
    ON c.table_name = t.table_name 
       and c.table_schema = t.table_schema
-WHERE c.column_name = 'playerid'
+WHERE c.column_name ilike '%team%' --'playerid'
       and t.table_schema not in ('information_schema', 'pg_catalog')
       and t.table_type = 'BASE TABLE'
 ORDER BY t.table_schema;
 
 -- Get Min height --
-select namelast,
+SELECT namelast,
 	   namefirst,
 	   height,
 	   playerid
-from public.people
-where height in ( select MIN(height)from public.people )
+FROM public.people
+WHERE height IN ( SELECT MIN(height)
+				  FROM public.people
+				)
 
-select g_all 
+SELECT g_all 
        ,app.teamid	   
       ,t.teamid 
 	  ,t.franchid 
 	  ,t.name
-from public.appearances app
-inner join public.teams t
-on app.teamid = t.teamid and 
+FROM public.appearances app
+INNER JOIN public.teams t
+ON app.teamid = t.teamid AND 
    app.yearid = t.yearid
-where playerid = 'gaedeed01'
+WHERE playerid = 'gaedeed01'
 ---------------------------------------------------------------------------------------------------------
 -- 3) Find all players in the database who played at Vanderbilt University. 
 --    Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
@@ -56,59 +58,61 @@ where playerid = 'gaedeed01'
 --    Which Vanderbilt player earned the most money in the majors? -- David Price
 
 -- create temp table of all player salary totals -- 
-select playerid, 
+SELECT playerid, 
        SUM(Salary) as totalSalary
-into temporary table playersalaries
-from salaries
+INTO temporary table playersalaries
+FROM salaries
 --where playerid = 'aardsda01'
 GROUP BY playerid
-order by playerid asc
+ORDER BY playerid ASC
 
 --drop table playersalaries
 
-select distinct 
+SELECT DISTINCT  
         p.namelast 
 	   ,p.namefirst
 	   ,CONCAT(p.namelast, ', ', p.namefirst) as player_full_name
 	   ,p.playerid
 	   ,s.schoolid 
 	   ,s.schoolname
-	   ,coalesce(ps.totalSalary,0) as totalSalary 
+	   ,COALESCE(ps.totalSalary,0) as totalSalary 
 	  
-from public.people p
-  left join public.collegeplaying c
-    on p.playerid = c.playerid
-  inner join schools s 
-    on c.schoolid = s.schoolid
-  left join playersalaries ps
-   on p.playerid = ps.playerid
-where s.schoolname = 'Vanderbilt University'
-order by totalSalary desc
+FROM public.people p
+  LEFT JOIN public.collegeplaying c
+    ON p.playerid = c.playerid
+  INNER JOIN schools s 
+    ON c.schoolid = s.schoolid
+  LEFT JOIN playersalaries ps
+   ON p.playerid = ps.playerid
+WHERE s.schoolname = 'Vanderbilt University'
+ORDER BY totalSalary DESC
+
+
 -------------------------------------------------------------------------------------------------------
 -- 4) Using the fielding table, group players into three groups based on their position: 
 --    label players with position OF as "Outfield", those with position "SS", "1B", "2B", and "3B" as "Infield", and those with position "P" or "C" as "Battery". 
 --    Determine the number of putouts made by each of these three groups in 2016.
 
 -- create the position groupings, put data into temp table
-select pos,
+SELECT pos,
        PO,
     CASE WHEN pos = 'OF' THEN 'Outfield'
-            WHEN pos IN ('SS','1B','2B','3B') then 'Infield'
-			WHEN pos IN ('P','C') then 'Battery'
-		else 'Unknown'
+            WHEN pos IN ('SS','1B','2B','3B') THEN 'Infield'
+			WHEN pos IN ('P','C') THEN 'Battery'
+		ELSE 'Unknown'
 		END  as PositionGroup
 		,yearid
-into temporary table PositionGroups 	
-from fielding
-where yearid = '2016'
+INTO temporary table PositionGroups 	
+FROM fielding
+WHERE yearid = '2016'
 
 -- select totals by positiongroup, year
-select PositionGroup, 
+SELECT PositionGroup, 
 	   SUM(po) as totalPutOuts,
 	   yearid
-from PositionGroups
-group by positiongroup, yearid 
-order by positiongroup ASC;
+FROM PositionGroups
+GROUP BY positiongroup, yearid 
+ORDER BY positiongroup ASC;
 
 --drop table PositionGroups
 ------------------------------------------------------------------------------------------------------------------
@@ -117,12 +121,12 @@ order by positiongroup ASC;
 --    Do the same for home runs per game. Do you see any trends? --- it shows an increasing trend for both with each decade
 
 SELECT --yearid, 
-      CONCAT(left(cast(yearid as varchar(4)), 3) ,'0') as decade,
+      CONCAT(left(cast(yearid as varchar(4)), 3) ,'0') AS decade,
 	  ROUND(SUM(g),2) as totalgames,
       ROUND(SUM(COALESCE(SO,0)),2) as totalstrikeouts,
-	  CAST(ROUND(SUM(COALESCE(SO,0)),2) / ROUND(SUM(g),2) as decimal(10,2)) as avgStrikeOutsPerGame
+	  CAST(ROUND(SUM(COALESCE(SO,0)),2) / ROUND(SUM(g),2) as decimal(10,2)) AS avgStrikeOutsPerGame
 FROM pitching
-where yearid >= '1920'
+WHERE yearid >= '1920'
 GROUP BY CONCAT(left(cast(yearid as varchar(4)), 3) ,'0')
 ORDER BY CONCAT(left(cast(yearid as varchar(4)), 3) ,'0') ASC
 
@@ -164,7 +168,8 @@ ORDER BY CONCAT(left(cast(yearid as varchar(4)), 3) ,'0') ASC
 
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? Seatlle Mariners, 116 Wins 
 --     What is the smallest number of wins for a team that did win the world series? - Toronto Blue Jays, 37 Wins
--- 	Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. - baseball strike in 1981, resulting in fewer games on average being played 
+-- 	Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. - 
+    baseball strike in 1981, resulting in fewer games on average being played 
 -- in the season. TOR played in 106 games that year, one less than the average 107.
 -- 	Then redo your query, excluding the problem year. 
 -- 	How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? 
@@ -224,34 +229,34 @@ ORDER BY CONCAT(left(cast(yearid as varchar(4)), 3) ,'0') ASC
    GROUP BY  teamid,
 			 name
    ORDER BY  MIN(W) ASC
-   
-   SELECT yearid,
-          MIN(teamid),
-	      MAX(W) as Wins
-   FROM teams
-   WHERE yearid BETWEEN '1970' and '2016'
-   GROUP BY yearid
-   order by yearid ASC
-   
-   select yearid, teamid, W, WSWin
-   from teams
-   WHERE yearid BETWEEN '1970' and '1974'
-    AND WSWin = 'Y'
-   ORDER by yearid ASC
-   
-    select yearid, Max(teamid), MAX(W)
-   from teams
-   WHERE yearid BETWEEN '1970' and '1974'
-    AND WSWin = 'Y'
-	GROUP BY yearid
-   ORDER by yearid ASC
-   
-   select yearid, teamid, W, WSWin
-   from teams
-   WHERE yearid BETWEEN '1970' and '1974'
-   -- AND WSWin = 'y'
-	--GROUP BY yearid
-   ORDER by yearid, W DESC
+  
+  -- part 2-- 
+   	
+	DROP TABLE WinWS
+	DROP TABLE MaxWins
+	
+	SELECT teamid as champ, 
+		   yearid, 
+		   w as champ_w
+	INTO temporary table WinWS
+	FROM teams 
+	WHERE 	(wswin = 'Y') 
+	AND (yearid BETWEEN 1970 AND 2016) 
+	
+	SELECT yearid, 
+		   max(w) as maxwins
+	INTO temporary table MaxWins
+	FROM teams
+	WHERE yearid BETWEEN 1970 AND 2016
+     GROUP BY yearid
+	
+	SELECT 	COUNT(*) AS all_years,
+			COUNT(CASE WHEN champ_w = maxwins THEN 'Yes' end) as max_wins_by_champ,
+			TO_CHAR((COUNT(CASE WHEN champ_w = maxw THEN 'Yes' end)/(COUNT(*))::real)* 100,'99.99%') as Percent
+	FROM 	WinWS 
+	LEFT JOIN MaxWins
+    USING(yearid)
+	
    ----------------------------------------------------------------------------------------------------------------------------------------------
   -- 8. Using the attendance figures from the homegames table, 
  -- find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). 
@@ -298,67 +303,180 @@ ORDER BY CONCAT(left(cast(yearid as varchar(4)), 3) ,'0') ASC
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --  9) Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? -- Bobby Cox 
 -- Give their full name and the teams that they were managing when they won the award.
-select * from public.awardsmanagers
-where awardid ilike '%TSN Manager%'
+SELECT * 
+FROM public.awardsmanagers
+WHERE awardid ILIKE '%TSN Manager%'
 
+SELECT distinct playerid, awardid, lgid
+FROM public.awardsmanagers
+WHERE awardid = 'TSN Manager of the Year'
+ORDER BY playerid
 
-select distinct playerid, awardid, lgid
-from public.awardsmanagers
-where awardid = 'TSN Manager of the Year'
---and  playerid = 'leylaji99' 
-and playerid = 'johnsda02'
-order by playerid
-
-
-select playerid, awardid, yearid,lgid
+-- AL league award winners
+SELECT playerid, awardid, yearid,lgid
 into temp table ALData
-from public.awardsmanagers
-where lgid = 'AL' 
-      and awardid = 'TSN Manager of the Year'
+FROM public.awardsmanagers
+WHERE lgid = 'AL' 
+      AND awardid = 'TSN Manager of the Year'
 
-select playerid, awardid, yearid,lgid
-into temp table NLData
-from public.awardsmanagers
-where lgid = 'NL' 
-and awardid = 'TSN Manager of the Year'
+-- NL league award winners
+SELECT playerid, awardid, yearid,lgid
+--into temp table NLData
+FROM public.awardsmanagers
+WHERE lgid = 'NL' 
+	AND awardid = 'TSN Manager of the Year'
 
-
+--    "leylaji99"
+--    "johnsda02"
+   
 -- drop table ALData
 -- DROP table NLData
+  
+ -- final data --  
+	SELECT DISTINCT p.playerid,
+			CONCAT(p.namelast, ', ', p.namefirst) as ManagerName,
+			al.yearid,
+			al.lgid,
+			m.teamid,
+			t.name
+	 FROM ALData al 	
+		INNER JOIN people p
+			ON al.playerid = p.playerid
+		LEFT JOIN managers m
+			ON p.playerid = m.playerid 
+				AND al.yearid = m.yearid  
+				AND al.lgid = m.lgid
+		INNER JOIN  teams t
+			ON m.teamid = t.teamid
+	WHERE p.playerid IN (
+						SELECT playerid
+						FROM ALData
+						INTERSECT
+						SELECT playerid
+						FROM NLData
+						)
+UNION
+	
+	SELECT DISTINCT p.playerid,
+			CONCAT(p.namelast, ', ', p.namefirst) as ManagerName,
+			nl.yearid,
+			nl.lgid,
+			m.teamid,
+			t.name
+	 FROM NLData nl
+	INNER JOIN people p
+			ON nl.playerid = p.playerid
+		LEFT JOIN managers m
+			ON p.playerid = m.playerid 
+				AND nl.yearid = m.yearid  
+				AND nl.lgid = m.lgid
+		INNER JOIN  teams t
+			ON m.teamid = t.teamid 
+				AND m.lgid = t.lgid 
+				AND m.yearid = t.yearid
+	WHERE p.playerid IN (
+						SELECT playerid
+						FROM ALData
+						INTERSECT
+						SELECT playerid
+						FROM NLData
+						)
+	 ORDER BY managername, yearid ASC
+	
+---------------------------------------------------------------------------------------------------------------------------------------------------
+--10).Analyze all the colleges in the state of Tennessee. Which college has had the most success in the major leagues. 
+--     Use whatever metric for success you like - number of players, number of games, salaries, world series wins, etc. 
+
+   
+   SELECT schoolid
+   		  ,schoolname 
+   INTO temp table TNColleges
+   FROM schools
+   WHERE schoolstate = 'TN'
+   ORDER BY schoolname ASC
+   
+   DROP TABLE TNColleges
+   SELECT * FROM TNColleges
    
    
-  -- final select -- 
- SELECT DISTINCT p.playerid,
- 		CONCAT(p.namelast, ', ', p.namefirst) as ManagerName,
- 		al.awardid, 
-		al.lgid,
-		al.yearid as ALYear, 
-		nl.playerid, 
-		nl.awardid	,
-		nl.yearid as NLYear,
-		nl.lgid
- FROM ALData al
- 	INNER JOIN NLData nl
- 		ON al.playerid = nl.playerid 
- 	INNER JOIN people p
-  		ON al.playerid = p.playerid
- ORDER BY 2
- 
-  SELECT * FROM ALData al
-  WHERE al.playerid = 'leylaji99'
-  UNION 
-  SELECT * FROM NLData nl
-  WHERE nl.playerid = 'leylaji99'
- 
- 
- -- Get team, league for each manager when won award(s).
-SELECT *
-FROM managers 
-WHERE playerid = 'leylaji99' and lgid = 'AL' and yearid = '1997'
+   SELECT count(WSWin) as WSWinCnt, 
+   		  teamid, 
+		  name
+		  rank
+   INTO temp table TotalWSWins
+   FROM teams 
+   GROUP BY teamid, name
+   ORDER BY rank desc
+   
+   select * from TotalWSWins
+   order by 1 DESC
+   
+   drop table TotalWSWins
+   
+   SELECT DISTINCT  
+        p.namelast 
+	   ,p.namefirst
+	   ,CONCAT(p.namelast, ', ', p.namefirst) as player_full_name
+	   ,p.playerid
+	   ,s.schoolid 
+	   ,s.schoolname
+	   ,t.name as teamname
+	   ,t.yearid
+	   ,COALESCE(ps.totalSalary,0) as totalSalary 
+	  
+FROM public.people p
+  
+  LEFT JOIN public.collegeplaying c
+    ON p.playerid = c.playerid
+	
+  INNER JOIN public.appearances a
+  	on p.playerid = a.playerid
 
-select * from teams
-where playerid = 'leylaji99' and lgid = 'AL'
+  INNER JOIN teams t
+   on a.teamid = a.teamid and a.lgid = t.lgid and a.yearid = t.yearid
+	
+  INNER JOIN TNColleges s 
+    ON c.schoolid = s.schoolid
+  
+  INNER JOIN playersalaries ps
+   ON p.playerid = ps.playerid
 
+WHERE t.WSWin = 'Y'
+ORDER BY totalSalary DESC
+   
 
---WHERE playerid = 'johnsda02'
+SELECT 
+        DISTINCT  
+        p.namelast 
+	   ,p.namefirst
+	   ,CONCAT(p.namelast, ', ', p.namefirst) as player_full_name
+	   ,p.playerid
+	   ,s.schoolid 
+	   ,s.schoolname
+	   --,t.name as teamname	   
+	   ,COALESCE(ps.totalSalary,0) as totalSalary 
+   
+FROM public.people p
+  
+  INNER JOIN public.collegeplaying c
+    ON p.playerid = c.playerid
+	
+  INNER JOIN public.appearances a
+  	on p.playerid = a.playerid
+
+  --INNER JOIN teams t
+  -- on a.teamid = a.teamid and a.lgid = t.lgid and a.yearid = t.yearid
+	
+  INNER JOIN TNColleges s 
+    ON c.schoolid = s.schoolid
+  
+  INNER JOIN playersalaries ps
+   ON p.playerid = ps.playerid
+
+ORDER BY totalSalary DESC
+
+select playerid, name, yearid
+from teams
+where playerid = 'h'
+   
    
